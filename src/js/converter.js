d0b4dac7cf866d64e1785153c33dc506c51fe287
Le Manh Cuong <le.manh.cuong-b@sun-asterisk.com>
2022-09-04 22:03:34 +0700

    add keyword

--- src/js/converter.js
+++ src/js/converter.js
@@ -6,7 +6,34 @@ var mode = "erb";
 var cfm = "";
 
 function replaceLateCommon(text){
-	return text.replaceAll("		", "	");
+  // common
+	text = text
+          .replaceAll("		", "	")
+          .replaceAll(/request.([\w]+)\.([\w]+)/g, "request[:$1][:$2]")
+          .replaceAll(/request.([\w]+)/g, "request[:$1]")
+          .replaceAll(" == 1", ".to_i == 1")
+          .replaceAll(" == 0", '.to_s == "0"')
+          .replaceAll(/structKeyExists\((.+)\) && |StructKeyExists\((.+)\) && /g, "")
+          .replaceAll(/isDefined\((.+)\) && /g, "")
+          .replaceAll(/ != ""|\.recordcount != 0/g, ".present?")
+          .replaceAll(/ == ""|\.recordcount == 0/g, ".blank?")
+
+  if (mode === "erb"){
+
+  }
+
+  if (mode === "rb"){
+    text = text
+          .replaceAll("&=", "+=")
+          .replaceAll(/<cfset (.+)>/g, "$1")
+          .replaceAll(/#([^#^ ^\n]+)#/g, "#{$1}")
+  }
+  
+  if (mode === "cfquery"){
+    
+  }
+
+  return text
 }
 
 window.handleChangeTaCfm = function handleChangeTaCfm() {
